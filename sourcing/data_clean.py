c6e58a64582ec3ad9ebb94454763c0c9c2dab59a
Max Zlotskiy <max.zlotskiy@gmail.com>
2021-12-03 10:56:20 -0500

    prototype of data sourcing

---
+++ sourcing/data_clean.py
@@ -0,0 +1,36 @@
+from os import listdir, getcwd
+from os.path import join, dirname
+import re
+
+DATA_PATH = "data"
+SPEAKER = re.compile("===")
+NEW_LINE = re.compile("\n\n")
+
+original_files = listdir(getcwd())
+next_file = 0
+first_file = 0
+
+#for each file in this directory
+for filename in original_files:
+	#Skip non-text files
+	if filename[-4:] != ".txt":
+		continue
+	#read the text file and try to split it
+	with open(filename, 'r') as file:
+		print("Opened " + filename)
+		transcript = re.sub(NEW_LINE, " ", file.read())
+		speakers = re.split(SPEAKER, transcript)
+		for s in speakers:
+			anon_name = "{:03d}.txt".format(next_file)
+			try:
+				individual_file = open(join("..", DATA_PATH, anon_name), 'w')
+				individual_file.write(s)
+				individual_file.close()
+			except:
+				print("Couldn't save file " + anon_name)
+			finally:
+				next_file += 1
+	print("{} files {} to {}".format(filename, first_file, next_file-1))
+	first_file = next_file
+
+print("Done")
