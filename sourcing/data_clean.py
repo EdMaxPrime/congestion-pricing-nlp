9e7629254bff341b3e47bacb46b1ed84c280a900
Stacey Li <listacey22@yahoo.com>
2021-12-04 15:40:48 -0500

    added explanation comments and created data directory

--- sourcing/data_clean.py
+++ sourcing/data_clean.py
@@ -1,19 +1,29 @@
-from os import listdir, getcwd
-from os.path import join, dirname
+from os import listdir, getcwd, mkdir
+from os.path import join, dirname, exists
 import re
 
 DATA_PATH = "data"
 SPEAKER = re.compile("===")
+# === allows to compare regexp to string
 NEW_LINE = re.compile("\n\n")
+# \n\n makes a newline
 
 original_files = listdir(getcwd())
+cwd = getcwd() 
+# listdir returns list containing the names of the entries in the directory given the path
+# getcwd copies the absolute pathname of the current working directory  
 next_file = 0
 first_file = 0
+
+# create data directory
+path = join(cwd, DATA_PATH)
+if not exists(path):
+	mkdir(path)
 
 #for each file in this directory
 for filename in original_files:
 	#Skip non-text files
-	if filename[-4:] != ".txt":
+	if filename[-4:] != ".txt": #if the last 4 chars in filename are not .txt
 		continue
 	#read the text file and try to split it
 	with open(filename, 'r') as file:
@@ -23,7 +33,7 @@ for filename in original_files:
 		for s in speakers:
 			anon_name = "{:03d}.txt".format(next_file)
 			try:
-				individual_file = open(join("..", DATA_PATH, anon_name), 'w')
+				individual_file = open(join(path, anon_name), 'w')
 				individual_file.write(s)
 				individual_file.close()
 			except:
