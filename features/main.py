773b54589b275a7a1c466154cd39196984ed2fb1
Max Zlotskiy <max.zlotskiy@gmail.com>
2021-12-06 10:38:37 -0500

    skeleton of framework

---
+++ features/main.py
@@ -0,0 +1,12 @@
+
+def main():
+	# read all files in data directory
+	# store them in a numpy array "corpus"
+	# process command line arguments
+	# call each feature function
+	# collate into one big result matrix
+	# interestingCorrelations()
+	# trainModel()
+
+if __name__ == "__main__":
+	main()
