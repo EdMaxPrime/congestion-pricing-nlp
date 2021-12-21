14b90402e54d297ee47fab967ce1c321ec2ee331
Stacey Li <listacey22@yahoo.com>
2021-12-21 14:25:56 -0500

    visualizations

--- features/correlations.py
+++ features/correlations.py
@@ -1,9 +1,16 @@
 from models import get_predictions
+import matplotlib.pyplot as plt
 
 # Inspect the features of the text and print interesting correlations
 def correlations():
 	data, headers, labels = get_predictions()
 	print(labels)
+	y_pronouns = data[:, headers.index("sentiment")]
+	plt.scatter(labels, y_pronouns)
+	plt.xlabel("Stance")
+	plt.ylabel("Compound Sentiment Scores")
+	plt.show()
+
 
 if __name__ == "__main__":
 	correlations()
